e6f0dae9a104699027d8cfd0d17e1633f9e86a5b
Komefumi <komefumi@protonmail.ch>
2021-11-26 20:11:37 +0530

    Create classnames utility to extract classnames as desired from css modules; Create supporting types

---
+++ src/scripts/types/loose.ts
@@ -0,0 +1,7 @@
+export interface IObjectStringKeys {
+  [key: string]: any;
+}
+
+export interface IObjectStringKeysAndValues {
+  [key: string]: string;
+}
---
+++ src/scripts/utils/classnames.ts
@@ -0,0 +1,18 @@
+import { IObjectStringKeysAndValues } from "@my-types/loose";
+
+export function generateCSSTransitionClassNamesForModule(
+  cssModuleItem: IObjectStringKeysAndValues,
+  baseClassName: string
+): IObjectStringKeysAndValues {
+  const be = `${baseClassName}_enter`;
+  const bea = `${be}_active}`;
+  const bed = `${be}_done`;
+  const bx = `${baseClassName}_exit`;
+  const bxa = `${bx}_active`;
+  const bxd = `${bx}_done`;
+
+  return [be, bea, bed, bx, bxa, bxd].reduce((mapping, className) => {
+    mapping[className] = cssModuleItem[className];
+    return mapping;
+  }, {} as IObjectStringKeysAndValues);
+}
