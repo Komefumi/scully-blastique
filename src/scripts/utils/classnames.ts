8ce4976efdc13dc6748d6703f835bfc85ad0202d
Komefumi <komefumi@protonmail.ch>
2021-11-26 21:56:24 +0530

    Fix: [In classnames.ts util] Generate values for the right property names

--- src/scripts/utils/classnames.ts
+++ src/scripts/utils/classnames.ts
@@ -4,15 +4,21 @@ export function generateCSSTransitionClassNamesForModule(
   cssModuleItem: IObjectStringKeysAndValues,
   baseClassName: string
 ): IObjectStringKeysAndValues {
+  const classes = cssModuleItem;
   const be = `${baseClassName}_enter`;
   const bea = `${be}_active`;
   const bed = `${be}_done`;
   const bx = `${baseClassName}_exit`;
   const bxa = `${bx}_active`;
   const bxd = `${bx}_done`;
+  const cssAsModule: IObjectStringKeysAndValues = {
+    enter: classes[be],
+    enterActive: classes[bea],
+    enterDone: classes[bed],
+    exit: classes[bx],
+    exitActive: classes[bxa],
+    exitDone: classes[bxd],
+  };
 
-  return [be, bea, bed, bx, bxa, bxd].reduce((mapping, className) => {
-    mapping[className] = cssModuleItem[className];
-    return mapping;
-  }, {} as IObjectStringKeysAndValues);
+  return cssAsModule;
 }
