23a7e0cc4951aa0e29904f41bd475ad04dcbf9bc
Komefumi <komefumi@protonmail.ch>
2021-12-01 02:56:47 +0530

    Bash helpers: Cleanup and preferred namings

--- local_bash_profile.sh
+++ local_bash_profile.sh
@@ -5,22 +5,17 @@ alias ssb="source ~/.bash_profile"
 
 export FZF_DEFAULT_COMMAND='fd --type f'
 
-vf() {
+open() {
   output=$(fzf)
   if [ -n "$output" ]; then
     vim $output
   fi
 }
 
-alias o="open"
-
-alias list="tree -I 'node_modules|dist'"
-alias l="list"
-alias t="touch"
+alias files="tree -I 'node_modules|dist'"
 search() {
   ag "$*"
 }
-alias s="search"
 
 
 # NOTE ! When searching for anything with quotes, remember to escape them as bash won't pass in the quotes directly otherwise
